2296ef95011f93103f29d4c6277cbf3442363b1c
Yoga Wira Pratama <yoga.wira@bps.go.id>
2021-12-21 10:02:00 +0700

    coba commit1

--- app/Pia.php
+++ app/Pia.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Pia extends Model
 {
     //
+    public function intervensi_khususes()
+    {
+        return $this->hasMany(IntervensiKhusus::class);
+    }
 }
